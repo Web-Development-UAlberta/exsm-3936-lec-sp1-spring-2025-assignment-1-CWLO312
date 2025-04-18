a02f9726693a7d822a43e2281ad9a4a5bdd73e4a
Ray Lo <lowai312@gmail.com>
2025-04-17 21:34:15 -0600

    recode class enginer; add getter and setter to validate input;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,30 +1,47 @@
 async function main() {
-//define enginer class
+//define engine class
   class engine {
     constructor(cylinderCount, isRunning) {
-      this.cylinderCount = cylinderCount;
-      this._isRunning = false;
+      this.cylinderCount = cylinderCount || 0;
+      this.isRunning = false;
     }
+    get cylinderCount() {
+      return this._cylinderCount;
+    }
+    set cylinderCount(cylinderCount) {
+          if (typeof cylinderCount !== "number" ||cylinderCount < 0 || cylinderCount % 1 !== 0) {
+           output ("Invalid cylinder count, it has to be a positive integer number. Defaulting to 0 cyclinder.");
+           this._cylinderCount = 0;
+          }
+          else {
+            this._cylinderCount = cylinderCount;
+          }        
+    }
+    
     get isRunning() {
       return this._isRunning;
     }
     set isRunning(isRunning) {
-      this._isRunning = isRunning;
+        if (isRunning !== true || isRunning !== false) {
+          output("Invalid engine running value, it has to be true or false.");
+        } else {
+          this._isRunning = isRunning;
+        }
+      }
     }
-  }
+  
 //define car class
     class car { 
-    constructor(make, model, year, odometer, engine) {
+    constructor(make, model, year, engine) {
       this.make = make;
       this.model = model;
       this.year = year;
-      odometer = 0;
-      this._odometer = odometer;            
+      this._odometer = 0;            
       this._engine = engine;
     }
 //define showInfo method
     showInfo() {
-      return `The car is ${this.make} ${this.model} from ${this.year}; the odometer is at ${this._odometer}. this engine has ${this._engine.cylinderCount} cylinders.`;
+      return `The car is ${this.make} ${this.model} from ${this.year}; the odometer is at ${this._odometer}. this engine has ${this._engine._cylinderCount} cylinders.`;
     }
 //define startEngine method
     startEngine() {
@@ -42,8 +59,8 @@ async function main() {
      return this._odometer;
     }
   }
-  const engine1 = new engine(4, false);
-  const myCar = new car("Toyota", "Camry", 2025, 500, engine1);
+  const engine1 = new engine(4, 3);
+  const myCar = new car("Toyota", "Camry", 2025, engine1);
   output(myCar.showInfo());
 
   output(myCar.startEngine());
